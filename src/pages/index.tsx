72cfe19e13ccc24cc17332723f219240d53cf989
Sandro <sandrox377@gmail.com>
2022-04-30 22:14:34 -0300

    mostrando post na tela

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,21 +24,62 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home({oi}) {
-  return <h1>{oi}</h1>
+export default function Home({postsPagination}:HomeProps) {
+
+  const posts = postsPagination.results
+  console.log(posts)
+  return (
+
+    <main>
+    <ul>
+      {
+        posts.map(post => (
+          <li key={post.uid}>
+            <strong>{post.data.title}</strong>
+            <p>{post.data.subtitle}</p>
+            <time>{post.first_publication_date}</time>
+            <span>{post.data.author}</span>
+          </li>)
+        )
+      }
+    </ul>
+    </main>
+  )
 }
+
+
+
 
 export const getStaticProps: GetStaticProps  = async () => {
   const prismic = getPrismicClient({});
   const postsResponse = await prismic.getByType('post',{
-    pageSize: 2
-  });
+      pageSize: 2
+    });
 
-  console.log(postsResponse)
+    console.log(JSON.stringify(postsResponse,null,2))
 
-  return ({
-    props:{
-      oi:"teste"
-    }}
+    const postsPagination = {
+      next_page: postsResponse.next_page,
+      
+        results: postsResponse.results.map((post)=> {
+          return {
+            uid: post.uid,
+            first_publication_date: post.first_publication_date,
+           data : {
+            title: post.data.title,
+            subtitle: post.data.subtitle,
+            author: post.data.author
+           }
+          }
+        })
+      }
+      console.log(postsPagination)
+
+  return (
+    {
+      props:{
+        postsPagination,
+      }
+    }
   )
-};
+}
